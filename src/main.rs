b81ad52dfda4254154620ffe133049449cc2f3f4
Huy <kingbazoka@gmail.com>
2017-05-21 14:38:36 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+extern crate syntect;
+use syntect::parsing::SyntaxSet;
+use syntect::highlighting::{ThemeSet, Style};
+use syntect::util::as_24_bit_terminal_escaped;
+use syntect::easy::HighlightFile;
+
+use std::io::BufRead;
+
+fn main() {
+    let ss = SyntaxSet::load_defaults_newlines(); // note we load the version with newlines
+    let ts = ThemeSet::load_defaults();
+
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Please pass in a file to highlight");
+        return;
+    }
+
+    let theme = &ts.themes["base16-eighties.dark"];
+    let mut highlighter = HighlightFile::new(&args[1], &ss, theme).unwrap();
+
+    // We use read_line instead of `for line in highlighter.reader.lines()` because that
+    // doesn't return strings with a `\n`, and including the `\n` gets us more robust highlighting.
+    // See the documentation for `SyntaxSet::load_syntaxes`.
+    // It also allows re-using the line buffer, which should be a tiny bit faster.
+    let mut line = String::new();
+    while highlighter.reader.read_line(&mut line).unwrap() > 0 {
+        {
+            let regions: Vec<(Style, &str)> = highlighter.highlight_lines.highlight(&line);
+            print!("{}", as_24_bit_terminal_escaped(&regions[..], false));
+        }
+        line.clear();
+    }
+}
